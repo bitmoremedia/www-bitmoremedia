6c001467888490e627469fc8c8363bffa12a92bb
Russ <russ@bitmoremedia.com>
2018-05-27 13:10:46 +0100

    remove the weird blue tint on the default grey-light color variable

--- src/tailwind/colors.js
+++ src/tailwind/colors.js
@@ -6,7 +6,7 @@ const colors = {
   'grey-darker': '#606f7b',
   'grey-dark': '#8795a1',
   grey: '#b8c2cc',
-  'grey-light': '#dae1e7',
+  'grey-light': '#e9e9e9',
   'grey-lighter': '#f1f5f8',
   'grey-lightest': '#f8fafc',
   white: '#ffffff',
