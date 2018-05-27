833e3716a51b9d96f92a7ece93619d568d067ecc
Russ <russ@bitmoremedia.com>
2018-05-27 13:01:53 +0100

    re-apply transition to hover effects on the the what we do cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -146,7 +146,7 @@ const WhatWeDoSection = () => {
                 }}
                 className="flex flex-col justify-center _what-we-do-card"
               >
-                <div className="bg-transparent-dark hover:bg-transparent-darker flex-1 flex flex-col justify-center">
+                <div className="bg-transparent-dark hover:bg-transparent-darker flex-1 flex flex-col justify-center _transition-all">
                   <div className="text-2xl md:text-xl lg:text-2xl pb-6">{item.title}</div>
                   <div
                     className="mx-auto text-base md:text-sm lg:text-base px-2"
