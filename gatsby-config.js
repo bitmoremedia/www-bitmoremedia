b16de7e8576f0ea69c7f4e9f28d7754d1a469c5a
Russ <russ@bitmoremedia.com>
2018-06-13 07:52:33 +0100

    upgrade to latest gatsby to get gatsby-source-filesystem working

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: `Bit More Media`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
