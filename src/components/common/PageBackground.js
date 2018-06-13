1156ab583e9f04705fd44733cc0d060d67b701ef
Russ <russ@bitmoremedia.com>
2018-06-13 09:15:05 +0100

    add poc of using an image to get the 'cover' effect that we need for page background; add polyfill for IE to support object fit cover

--- src/components/common/PageBackground.js
+++ src/components/common/PageBackground.js
@@ -2,12 +2,21 @@ import React from 'react'
 
 const PageBackground = ({ bgImgSrc, overlayBackground = 'rgba(2, 2, 2, 0.5)', children }) => {
   return (
-    <div
-      className="min-h-screen relative bg-center bg-no-repeat bg-cover h-full flex flex-col"
-      style={{
-        backgroundImage: `url(${bgImgSrc})`,
-      }}
-    >
+    <div className="min-h-screen relative h-full flex flex-col">
+      <img
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: -1,
+          objectFit: 'cover',
+          // below is a hack to support ie (also requires the object-fit-images polyfill)
+          fontFamily: 'object-fit: cover',
+        }}
+        src={bgImgSrc}
+      />
       <div
         className="flex flex-col flex-1"
         style={{
